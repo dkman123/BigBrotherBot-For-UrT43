0a33f9683b19296e9fc13cdb037cdfcf43a2bb6d
dkman123 <dkman123@hotmail.com>
2022-12-10 12:28:39 -0500

    weapon damage descriptions added

--- b3/sql/mysql/iso_weapon.sql
+++ b3/sql/mysql/iso_weapon.sql
@@ -6,44 +6,44 @@ CREATE TABLE IF NOT EXISTS `iso_weapon` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- insert records
-INSERT INTO `iso_weapon` (id, description) VALUES ('1',  'MOD_WATER');
-INSERT INTO `iso_weapon` (id, description) VALUES ('3',  'MOD_LAVA');
-INSERT INTO `iso_weapon` (id, description) VALUES ('5',  'MOD_TELEFRAG');
-INSERT INTO `iso_weapon` (id, description) VALUES ('6',  'MOD_FALLING');
-INSERT INTO `iso_weapon` (id, description) VALUES ('7',  'MOD_SUICIDE');
-INSERT INTO `iso_weapon` (id, description) VALUES ('9',  'MOD_TRIGGER_HURT');
-INSERT INTO `iso_weapon` (id, description) VALUES ('10', 'MOD_CHANGE_TEAM');
-INSERT INTO `iso_weapon` (id, description) VALUES ('12', 'KNIFE');
-INSERT INTO `iso_weapon` (id, description) VALUES ('13', 'KNIFE_THROWN');
-INSERT INTO `iso_weapon` (id, description) VALUES ('14', 'BERETTA');
-INSERT INTO `iso_weapon` (id, description) VALUES ('15', 'DEAGLE');
-INSERT INTO `iso_weapon` (id, description) VALUES ('16', 'SPAS');
-INSERT INTO `iso_weapon` (id, description) VALUES ('17', 'UMP45');
-INSERT INTO `iso_weapon` (id, description) VALUES ('18', 'MP5K');
-INSERT INTO `iso_weapon` (id, description) VALUES ('19', 'LR300');
-INSERT INTO `iso_weapon` (id, description) VALUES ('20', 'G36');
-INSERT INTO `iso_weapon` (id, description) VALUES ('21', 'PSG1');
-INSERT INTO `iso_weapon` (id, description) VALUES ('22', 'HK69');
-INSERT INTO `iso_weapon` (id, description) VALUES ('23', 'BLED');
-INSERT INTO `iso_weapon` (id, description) VALUES ('24', 'KICKED');
-INSERT INTO `iso_weapon` (id, description) VALUES ('25', 'HE_GRENADE');
-INSERT INTO `iso_weapon` (id, description) VALUES ('28', 'SR8');
-INSERT INTO `iso_weapon` (id, description) VALUES ('30', 'AK103');
-INSERT INTO `iso_weapon` (id, description) VALUES ('31', 'SPLODED');
-INSERT INTO `iso_weapon` (id, description) VALUES ('32', 'SLAPPED');
-INSERT INTO `iso_weapon` (id, description) VALUES ('33', 'SMITED');
-INSERT INTO `iso_weapon` (id, description) VALUES ('34', 'BOMBED');
-INSERT INTO `iso_weapon` (id, description) VALUES ('35', 'NUKED');
-INSERT INTO `iso_weapon` (id, description) VALUES ('36', 'NEGEV');
-INSERT INTO `iso_weapon` (id, description) VALUES ('37', 'HK69_HIT');
-INSERT INTO `iso_weapon` (id, description) VALUES ('38', 'M4');
-INSERT INTO `iso_weapon` (id, description) VALUES ('39', 'GLOCK');
-INSERT INTO `iso_weapon` (id, description) VALUES ('40', 'COLT1911');
-INSERT INTO `iso_weapon` (id, description) VALUES ('41', 'MAC11');
-INSERT INTO `iso_weapon` (id, description) VALUES ('42', 'FRF1');
-INSERT INTO `iso_weapon` (id, description) VALUES ('43', 'BENELLI');
-INSERT INTO `iso_weapon` (id, description) VALUES ('44', 'P90');
-INSERT INTO `iso_weapon` (id, description) VALUES ('45', 'MAGNUM');
-INSERT INTO `iso_weapon` (id, description) VALUES ('46', 'TOD50');
-INSERT INTO `iso_weapon` (id, description) VALUES ('47', 'FLAG');
-INSERT INTO `iso_weapon` (id, description) VALUES ('48', 'GOOMBA');
+INSERT INTO `iso_weapon` (id, description) VALUES ('1',  'MOD_WATER');  -- drowning
+INSERT INTO `iso_weapon` (id, description) VALUES ('3',  'MOD_LAVA');  -- lava
+INSERT INTO `iso_weapon` (id, description) VALUES ('5',  'MOD_TELEFRAG');  -- having someone teleport on top of you
+INSERT INTO `iso_weapon` (id, description) VALUES ('6',  'MOD_FALLING');  -- falling damage
+INSERT INTO `iso_weapon` (id, description) VALUES ('7',  'MOD_SUICIDE');  -- /kill command
+INSERT INTO `iso_weapon` (id, description) VALUES ('9',  'MOD_TRIGGER_HURT');  -- finding well placed hurt
+INSERT INTO `iso_weapon` (id, description) VALUES ('10', 'MOD_CHANGE_TEAM');  -- changing team
+INSERT INTO `iso_weapon` (id, description) VALUES ('12', 'KNIFE');  -- knife
+INSERT INTO `iso_weapon` (id, description) VALUES ('13', 'KNIFE_THROWN');  -- thrown knife
+INSERT INTO `iso_weapon` (id, description) VALUES ('14', 'BERETTA');  -- baretta pistol
+INSERT INTO `iso_weapon` (id, description) VALUES ('15', 'DEAGLE');  -- DE pistol
+INSERT INTO `iso_weapon` (id, description) VALUES ('16', 'SPAS');  -- spas shotgun
+INSERT INTO `iso_weapon` (id, description) VALUES ('17', 'UMP45');  -- ump secondary
+INSERT INTO `iso_weapon` (id, description) VALUES ('18', 'MP5K');  -- mp5k secondary
+INSERT INTO `iso_weapon` (id, description) VALUES ('19', 'LR300');  -- lr300 primary
+INSERT INTO `iso_weapon` (id, description) VALUES ('20', 'G36');  -- g36 primary
+INSERT INTO `iso_weapon` (id, description) VALUES ('21', 'PSG1');  -- psg1 sniper rifle
+INSERT INTO `iso_weapon` (id, description) VALUES ('22', 'HK69');  -- hk69 explosion
+INSERT INTO `iso_weapon` (id, description) VALUES ('23', 'BLED');  -- bleed damage
+INSERT INTO `iso_weapon` (id, description) VALUES ('24', 'KICKED');  -- player kick/boot
+INSERT INTO `iso_weapon` (id, description) VALUES ('25', 'HE_GRENADE');  -- HE grenade
+INSERT INTO `iso_weapon` (id, description) VALUES ('28', 'SR8');  -- sr8 sniper rifle
+INSERT INTO `iso_weapon` (id, description) VALUES ('30', 'AK103');  -- ak primary
+INSERT INTO `iso_weapon` (id, description) VALUES ('31', 'SPLODED');  -- self HE grenade in hand
+INSERT INTO `iso_weapon` (id, description) VALUES ('32', 'SLAPPED');  -- slap rcon command
+INSERT INTO `iso_weapon` (id, description) VALUES ('33', 'SMITED');  -- smite rcon command
+INSERT INTO `iso_weapon` (id, description) VALUES ('34', 'BOMBED');  -- probably bomb mode explosion
+INSERT INTO `iso_weapon` (id, description) VALUES ('35', 'NUKED');  -- nuke rcon command
+INSERT INTO `iso_weapon` (id, description) VALUES ('36', 'NEGEV');  -- negev primary
+INSERT INTO `iso_weapon` (id, description) VALUES ('37', 'HK69_HIT');  -- hk69 direct hit
+INSERT INTO `iso_weapon` (id, description) VALUES ('38', 'M4');  -- m4 primary
+INSERT INTO `iso_weapon` (id, description) VALUES ('39', 'GLOCK');  -- glock pistol
+INSERT INTO `iso_weapon` (id, description) VALUES ('40', 'COLT1911'); -- colt 1911 pistol
+INSERT INTO `iso_weapon` (id, description) VALUES ('41', 'MAC11');  -- mac11 secondary
+INSERT INTO `iso_weapon` (id, description) VALUES ('42', 'FRF1');  -- frf1 sniper rifle
+INSERT INTO `iso_weapon` (id, description) VALUES ('43', 'BENELLI');  -- benelli shotgun secondary
+INSERT INTO `iso_weapon` (id, description) VALUES ('44', 'P90');  -- p90 secondary
+INSERT INTO `iso_weapon` (id, description) VALUES ('45', 'MAGNUM');  -- magnum pistol
+INSERT INTO `iso_weapon` (id, description) VALUES ('46', 'TOD50');  -- the instagib rifle
+INSERT INTO `iso_weapon` (id, description) VALUES ('47', 'FLAG');  -- hot potato, flag explosion
+INSERT INTO `iso_weapon` (id, description) VALUES ('48', 'GOOMBA');  -- curb stomp, landing on someone's head
