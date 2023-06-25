5335d974b75b453f63a1be36a8c017fcfc2898f6
dkman123 <dkman123@hotmail.com>
2023-06-25 07:49:16 -0400

    Revert b3-update-1.10.3.sql, it was accidentally overwritten

--- b3/sql/mysql/b3-update-1.10.3.sql
+++ b3/sql/mysql/b3-update-1.10.3.sql
@@ -1,5 +1,9 @@
--- SQL code to update default B3 database tables to B3 version 1.10.4 --
+-- SQL code to update default B3 database tables to B3 version 1.10.3 --
 -- --------------------------------------------------------
 
-ALTER TABLE `clients` ADD `permmute` TINYINT(1) NOT NULL DEFAULT '0';
+-- Alter the login field of the client table (email can be no longer than 254 characters)
+ALTER TABLE `clients` ADD `app` VARCHAR(32) NOT NULL DEFAULT '';
+
+-- the country 2 letter iso code
+ALTER TABLE `clients` ADD `isocode` CHAR(2) NOT NULL DEFAULT '';
 
